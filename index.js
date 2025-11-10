99442e31f554a3dd9e211297859fa6951f286cbb
stivo12-k <kunangkunang139@gmail.com>
2025-11-10 22:01:14 +0800

    frist

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import SplashScreen from './src/pages/SplashScreen';
 import SignIn from './src/pages/SignIn';
-
-AppRegistry.registerComponent(appName, () => SignIn);
+import App from './App';
+AppRegistry.registerComponent(appName, () => App);
